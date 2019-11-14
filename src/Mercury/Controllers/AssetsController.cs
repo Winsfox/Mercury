acd9f1ebd988b0a3fa692d5879d0c166be035cfc
Artem <artem.avivov@gmail.com>
2019-11-14 07:23:23 +0300

    Добавил бекенд AspMvc

---
+++ src/Mercury/Controllers/AssetsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Mercury.Controllers
+{
+    public class AssetsController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public IEnumerable<string> GetByIsin()
+        {
+            return new string[] { "value3", "value4" };
+        }
+    }
+}
